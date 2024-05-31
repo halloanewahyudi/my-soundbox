9a793daf888bd853d7cdc50766930258e1221bfe
hallo.anewahyudi <hallo.anewahyudi@gmail.com>
2024-05-31 01:43:07 +0000

    lormipsum

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { animate, timeline } from 'motion';
+import { onMounted, ref } from 'vue';
+const title = ref(null)
+const text = ref(null)
+const sequence = [
+  [title.value, { y: 100 }, { at: 0.5 }],
+  // This will start 0.5 from the start of the whole timeline:
+  [text.value, { x: 100 }, { at: 0.5 }]
+]
+onMounted(() => {
+  timeline(sequence)
+})
+
+</script>
+<template>
+  <div class="home ">
+    <div class="container mx-auto">
+    <div class="grid grid-cols-1 sm:grid-cols-2 min-h-screen items-center">
+      <div class="p-4 cover-text">
+           <h1 ref="title"class="text-[80px] font-bold leading-none"> QRIS Soundbox </h1>
+           <p ref="text" class="text-xl">
+            meningkatkan efisiensi dan keamanan transaksi bisnis Anda, serta memberikan pengalaman pembayaran terbaik bagi pelanggan Anda.
+           </p>
+      </div>
+      <div class="md:pt-10">
+        <img src="../assets/img/device.png" alt="">
+      </div>
+    </div>
+    </div>
+</div>
+</template>
+<style scoped>
+ .home{
+  background:url('../assets/lingkar.svg') no-repeat ;
+  background-size:contain
+  
+ }
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container:{
+       center:true,
+       padding:'2rem'
+    },
+    extend: {
+      colors:{
+        'brand': {
+          '50': '#ecf4ff',
+          '100': '#dceaff',
+          '200': '#c0d8ff',
+          '300': '#9abdff',
+          '400': '#7295ff',
+          '500': '#516fff',
+          '600': '#4153fa',
+          '700': '#2533dd',
+          '800': '#212eb2',
+          '900': '#232f8c',
+          '950': '#151a51',
+      },
+      
+      }
+    },
   },
   plugins: [],
 }
