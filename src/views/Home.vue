d9949adc55433888899dc3e5835264dfbc09cf13
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-05-31 09:02:38 +0700

    animate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,37 +3,51 @@ import { animate, timeline } from 'motion';
 import { onMounted, ref } from 'vue';
 const title = ref(null)
 const text = ref(null)
-const sequence = [
-  [title.value, { y: 100 }, { at: 0.5 }],
-  // This will start 0.5 from the start of the whole timeline:
-  [text.value, { x: 100 }, { at: 0.5 }]
-]
+const device = ref(null)
+
+const welcome = () => {
+  const sequence = [
+    [title.value, {
+      opacity: [0, 1],
+      x: [-200, 0],
+      scale: [.3, 1]
+    }],
+    // This will start 0.5 from the start of the whole timeline:
+    [text.value, { opacity: [0, 1], y: [100, 0] }],
+    [
+      '.device', { opacity: [0, 1], scale: [.2, 1] }
+    ]
+
+  ]
+  timeline(sequence, { duration: 2, delay: .3 })
+}
+
 onMounted(() => {
-  timeline(sequence)
+  welcome()
 })
 
 </script>
 <template>
   <div class="home ">
     <div class="container mx-auto">
-    <div class="grid grid-cols-1 sm:grid-cols-2 min-h-screen items-center">
-      <div class="p-4 cover-text">
-           <h1 ref="title"class="text-[80px] font-bold leading-none"> QRIS Soundbox </h1>
-           <p ref="text" class="text-xl">
-            meningkatkan efisiensi dan keamanan transaksi bisnis Anda, serta memberikan pengalaman pembayaran terbaik bagi pelanggan Anda.
-           </p>
-      </div>
-      <div class="md:pt-10">
-        <img src="../assets/img/device.png" alt="">
+      <div class="grid grid-cols-1 sm:grid-cols-2 min-h-screen items-center">
+        <div class="p-4 cover-text">
+          <h1 ref="title" class="text-[80px] font-bold leading-none"> QRIS Soundbox </h1>
+          <p ref="text" class="text-2xl">
+            meningkatkan efisiensi dan keamanan transaksi bisnis Anda, serta memberikan pengalaman pembayaran terbaik
+            bagi pelanggan Anda.
+          </p>
+        </div>
+        <div class="md:pt-10">
+          <img class="device" src="../assets/img/device.png" alt="">
+        </div>
       </div>
     </div>
-    </div>
-</div>
+  </div>
 </template>
 <style scoped>
- .home{
-  background:url('../assets/lingkar.svg') no-repeat ;
-  background-size:contain
-  
- }
+.home {
+  background: url('../assets/lingkar.svg') no-repeat;
+  background-size: contain
+}
 </style>
