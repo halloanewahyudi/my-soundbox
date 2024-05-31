e8b4cea96a8e7cf3b5392c5f0c74f9e058644529
hallo.anewahyudi <hallo.anewahyudi@gmail.com>
2024-05-31 02:19:59 +0000

    lorem

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,8 @@
 
 </script>
 <template>
-    <main class="min-h-screen bg-gradient-to-b from-brand-600 to-brand-950 text-brand-50">
+    <main class=" min-h-screen bg-gradient-to-b from-brand-600 to-brand-950 text-brand-50">
         <RouterView />
     </main>
 </template>
-
-
 
